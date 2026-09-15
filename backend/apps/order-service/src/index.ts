cfbdacfeee263fc3ad8342e886d8db7ebf64e09d
Kunal Singh <kunalindia59@gmail.com>
2026-09-15 20:52:19 +0530

    Type fix

--- backend/apps/order-service/src/index.ts
+++ backend/apps/order-service/src/index.ts
@@ -17,42 +17,6 @@ const redisClient = await getRedisClient()
 console.log('Connected to redis')
 
 
-
-
-await redisClient.subscribe('UPDATE_ORDER', async (message) => {
-    try {
-        const subscribedData = JSON.parse(message) as { eventId: string, orderId: string, userId: string, eventType: string, productIds: string[] }
-        console.log('ORDER UPDATE DATA IS : ', subscribedData)
-        // update the order
-        switch (subscribedData.eventType) {
-            case "PAYMENT_CONFIRMED":
-                await prisma.$transaction(async (tx) => {
-                    const res = await tx.order.updateMany(
-                        {
-                            where: {
-                                id: subscribedData.orderId
-                            },
-                            data: {
-                                status: "CONFIRMED"
-                            }
-                        }
-                    )
-                    if (res.count > 1) {
-                        await redisClient.publish("INVENTORY_UPDATE", JSON.stringify({ orderId: subscribedData.orderId, userId: subscribedData.userId, productIds: subscribedData.productIds }))
-                    }
-
-                }, { maxWait: 5000, timeout: 10000 })
-                break;
-            case "CREATE_TRACKING":
-
-                break;
-        }
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-
 async function pickOrders() {
     const objectFromQueue = await redisClient.brPop('ORDERS', 0)
     console.log('Object Recieved in Order Service : ', JSON.parse(objectFromQueue!.element))
