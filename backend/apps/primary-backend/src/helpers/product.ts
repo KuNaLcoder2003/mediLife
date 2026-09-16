bd3146390c9c3be78d1cb6af885f35464cd1e4eb
Kunal Singh <kunalindia59@gmail.com>
2026-09-16 12:42:49 +0530

    Helper functions

--- backend/apps/primary-backend/src/helpers/product.ts
+++ backend/apps/primary-backend/src/helpers/product.ts
@@ -1,1 +1,34 @@
 import { prisma } from "@repo/db";
+export const fetchProductById = async (productId: string) => {
+    const product = await prisma.products.findUnique({
+        where: {
+            id: productId
+        },
+        select: {
+            productName: true,
+            id: true,
+            productDescription: true,
+            quantity: true,
+            price: true,
+            discount: true,
+            images: {
+                select: {
+                    imageUrl: true,
+                    id: true
+                }
+            },
+            category: {
+                select: {
+                    category: true
+                }
+            }
+        }
+    })
+
+    if (!product) {
+        return false
+    }
+    else {
+        return product
+    }
+}
