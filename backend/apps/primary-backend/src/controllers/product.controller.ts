9871113acfdc239cd0ec4c652f8ce9a0fb8a1df6
Kunal Singh <kunalindia59@gmail.com>
2026-09-16 12:42:13 +0530

    Route handlers for new product routes

--- backend/apps/primary-backend/src/controllers/product.controller.ts
+++ backend/apps/primary-backend/src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
 import express from "express"
-import type { ProductDetails } from "../types/index.js"
+import type { keyWords, ProductDetails } from "../types/index.js"
 import { prisma } from "@repo/db"
+import { fetchProductById } from "../helpers/product.js"
 
 export const addProductHandler = async (req: express.Request, res: express.Response) => {
     try {
@@ -81,6 +82,102 @@ export const addNewProductCategory = async (req: express.Request, res: express.R
         console.log(error)
         res.status(500).json({
             message: "Something went wrong",
+            valid: false
+        })
+    }
+}
+
+export const getProducts = async (req: express.Request, res: express.Response) => {
+    try {
+        const keWords: keyWords = req.query as any
+        if (!keWords) {
+            res.status(400).json({
+                message: 'Please enter product name , category to search',
+                valid: false
+            })
+            return
+        }
+        const products = await prisma.products.findMany({
+            where: {
+                OR: [
+                    {
+                        productName: {
+                            startsWith: keWords.name,
+                        }
+                    },
+                    {
+                        productName: {
+                            startsWith: keWords?.category ? keWords.category : ""
+                        }
+                    }
+                ]
+            },
+            select: {
+                productName: true,
+                id: true,
+                productDescription: true,
+                quantity: true,
+                price: true,
+                discount: true,
+                images: {
+                    select: {
+                        imageUrl: true,
+                        id: true
+                    }
+                },
+                category: {
+                    select: {
+                        category: true
+                    }
+                }
+            }
+        })
+        if (!products) {
+            res.status(404).json({
+                message: "No products found",
+                valid: false
+            })
+            return
+        }
+        res.status(200).json({
+            valid: true,
+            products
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Somnething went wrong",
+            valid: false
+        })
+    }
+}
+
+export const getProductById = async (req: express.Request, res: express.Response) => {
+    try {
+        const productId = req.params.productId as string
+        if (!productId) {
+            res.status(400).json({
+                message: 'Please select a product',
+                valid: false
+            })
+            return
+        }
+        const product = await fetchProductById(productId)
+        if (!product) {
+            res.status(404).json({
+                message: "Unable to find product",
+                valid: false
+            })
+            return
+        }
+        res.status(200).json({
+            valid: true,
+            product
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Somnething went wrong",
             valid: false
         })
     }
