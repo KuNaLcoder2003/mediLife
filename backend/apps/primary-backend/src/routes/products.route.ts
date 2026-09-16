391821f7d4ffd84e32561cb4c21a8286ba409575
Kunal Singh <kunalindia59@gmail.com>
2026-09-16 12:41:48 +0530

    New product routes

--- backend/apps/primary-backend/src/routes/products.route.ts
+++ backend/apps/primary-backend/src/routes/products.route.ts
@@ -1,9 +1,12 @@
 import express from "express"
-import { addNewProductCategory, addProductHandler } from "../controllers/product.controller.js"
+import { addNewProductCategory, addProductHandler, getProducts } from "../controllers/product.controller.js"
+import { getProductById } from "../controllers/product.controller.js"
 
 const productsRouter = express.Router()
 
 productsRouter.post('/newProduct', addProductHandler)
 productsRouter.post('/newCategory', addNewProductCategory)
+productsRouter.get('/', getProducts)
+productsRouter.get('/:productId', getProductById)
 
 export default productsRouter
