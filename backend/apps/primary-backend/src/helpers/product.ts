cfada0f01ce57a6f41e958853e634875d987b5cb
Kunal Singh <kunalindia59@gmail.com>
2026-09-17 15:20:37 +0530

    helper functions for cloud uploads and uploading multiple assets

--- backend/apps/primary-backend/src/helpers/product.ts
+++ backend/apps/primary-backend/src/helpers/product.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@repo/db";
+import { uploadAsset } from "../utility/cloud.js";
 export const fetchProductById = async (productId: string) => {
     const product = await prisma.products.findUnique({
         where: {
@@ -31,4 +32,59 @@ export const fetchProductById = async (productId: string) => {
     else {
         return product
     }
+}
+
+export const createProductImage = (data: { imageUrl: string, key: string, cloudId: string, productId: string }) => {
+    return prisma.productIamges.create({
+        data: data
+    })
+}
+
+export const createMultipleImages = async (images: { imageUrl: string, key: string, cloudId: string, productId: string }[]) => {
+    const result = await Promise.allSettled(images.map(async (obj) => {
+        const entry = await createProductImage(obj)
+        return entry
+    }))
+
+    let notCreated = []
+    for (let i = 0; i < result.length; i++) {
+        if (result[i]?.status == "rejected") {
+            notCreated.push(images[i])
+        }
+    }
+    if (notCreated.length > 0) {
+        return {
+            notCreated,
+            valid: false
+        }
+    }
+    return {
+        valid: true,
+    }
+}
+
+export const uploadMultipleAssetsToCloud = async (files: { fileBuffer: Buffer, fileName: string }[], productId: string) => {
+    let flag = false
+    let result: { imageUrl: string, key: string, cloudId: string, productId: string }[] = []
+    if (!files) {
+        flag = false
+        return {
+            valid: flag,
+            uploaded: []
+        }
+    }
+
+    for (let file of files) {
+        const res = await uploadAsset(file.fileBuffer, file.fileName)
+        if (!res.valid) {
+            flag = false
+            break
+        }
+        result.push({ imageUrl: res.url!, key: res.key as string, cloudId: res.key as string, productId: productId })
+    }
+    return {
+        valid: flag,
+        uploaded: result
+    }
+
 }
