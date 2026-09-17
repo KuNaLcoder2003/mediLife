ad409bb8f38e698e6e462d1f71670ac9804e9463
Kunal Singh <kunalindia59@gmail.com>
2026-09-17 15:15:57 +0530

    Adding the productRouter to the sub router

--- backend/apps/primary-backend/src/routes/routes.ts
+++ backend/apps/primary-backend/src/routes/routes.ts
@@ -2,10 +2,12 @@ import express from "express"
 import usersRouter from "./users.route.js"
 import authRouter from "./auth.routes.js"
 import ordersRouter from "./order.route.js"
+import productsRouter from "./products.route.js"
 
 const router = express.Router()
 router.use('/users', usersRouter)
 router.use('/auth', authRouter)
 router.use('/order', ordersRouter)
+router.use('/product', productsRouter)
 
 export default router
