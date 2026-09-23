7db5603b741a305340a86e18572fe64e9e9025f5
Kunal Singh <kunalindia59@gmail.com>
2026-09-23 23:56:25 +0530

    New route when an order is cancelled

--- backend/apps/primary-backend/src/routes/order.route.ts
+++ backend/apps/primary-backend/src/routes/order.route.ts
@@ -4,5 +4,6 @@ import { newOrder } from "../controllers/orders.controller.js"
 const ordersRouter = express.Router()
 
 ordersRouter.post('/newOrder', newOrder)
+ordersRouter.post('/cancel')
 
 export default ordersRouter
