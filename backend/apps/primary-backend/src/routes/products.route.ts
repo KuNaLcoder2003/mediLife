915ca5efb8a3d893d406cdadc4214554f18f2284
Kunal Singh <kunalindia59@gmail.com>
2026-09-24 15:10:14 +0530

    New route

--- backend/apps/primary-backend/src/routes/products.route.ts
+++ backend/apps/primary-backend/src/routes/products.route.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { addNewProductCategory, addProductHandler, getProducts } from "../controllers/product.controller.js"
+import { addNewProductCategory, addProductHandler, getProducts, uploadImagesHandler } from "../controllers/product.controller.js"
 import { getProductById } from "../controllers/product.controller.js"
 import multer from "multer"
 const memoryStorage = multer.memoryStorage()
@@ -11,5 +11,5 @@ productsRouter.post('/newProduct', addProductHandler)
 productsRouter.post('/newCategory', addNewProductCategory)
 productsRouter.get('/', getProducts)
 productsRouter.get('/:productId', getProductById)
-productsRouter.post('/newImages', upload.array("product_images", 5))
+productsRouter.post('/newImages', upload.array("product_images", 5), uploadImagesHandler)
 export default productsRouter
